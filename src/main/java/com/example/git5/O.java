13f936f82672a122e0af2972a87f5f8464de61fd
Elvin-Bayramov <bayramov.elvin@yahoo.com>
2024-08-05 14:13:13 +0400

    changed

--- src/main/java/com/example/git5/O.java
+++ src/main/java/com/example/git5/O.java
@@ -2,6 +2,9 @@ package com.example.git5;
 
 public class O {
 
+    //    3
+//    4
+
     //    5
 //    6
 }
--- src/main/java/com/example/git5/S.java
+++ src/main/java/com/example/git5/S.java
@@ -4,4 +4,5 @@ public class S {
 //    single responsibility logic
 //    line for first commit
 //    line for second commit
+//    implemented new feature
 }
